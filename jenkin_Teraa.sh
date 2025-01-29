f25556a8b38e551b86f480d2e2b431b7343d0d66
Gaurav-8281 <gauravpatil70450@gmail.com>
2025-01-29 11:43:11 +0530

    Update jenkin_Teraa.sh

--- jenkin_Teraa.sh
+++ jenkin_Teraa.sh
@@ -2,21 +2,16 @@
 # Jenkins Installation
 ################################################################################################
 
-apt update -y  # It will update repo 
-apt install openjdk-17-jre -y # It will install Openjdk
+sudo apt update -y
+sudo apt install fontconfig openjdk-17-jre -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
 
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null  # This will update the repository for jenkins
-
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null  # This will update the repository for jenkins
-
-apt-get update -y # This will update the repository
-
-apt-get install jenkins -y # This will install jenkins
-
-echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 #####################################################################################################
 # Terraform Installation
