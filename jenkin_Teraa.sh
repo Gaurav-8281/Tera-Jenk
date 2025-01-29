b66a2075fa3910eaff351f0f9633737ddf657b57
Gaurav-8281 <gauravpatil70450@gmail.com>
2025-01-29 11:22:23 +0530

    Create jenkin_Teraa.sh

---
+++ jenkin_Teraa.sh
@@ -0,0 +1,39 @@
+################################################################################################
+# Jenkins Installation
+################################################################################################
+
+apt update -y  # It will update repo 
+apt install openjdk-17-jre -y # It will install Openjdk
+
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null  # This will update the repository for jenkins
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null  # This will update the repository for jenkins
+
+apt-get update -y # This will update the repository
+
+apt-get install jenkins -y # This will install jenkins
+
+echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+#####################################################################################################
+# Terraform Installation
+#####################################################################################################
+
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    gpg --dearmor | \
+    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+gpg --no-default-keyring \
+    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+    --fingerprint
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update -y
+
+sudo apt install terraform -y
